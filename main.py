fef5054e3f45aeaec05d1ad1e76295d9b91d2c45
Trevor French <42419234+TrevorFrench@users.noreply.github.com>
2022-02-16 21:27:12 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # TITLE: Messari On-Chain Data Challenge
-# DESCRIPTION: Uses web3.py to accomplish several tasks
+# DESCRIPTION: Uses web3.py to query ETH balance, ENS balance,
+#   as well as transaction history for a given array of public 
+#   addresses
 # TODO:
-# -
-# -
-# -
+# - See if there is a way to scale up transaction fetching
+# - Output results into dataframes
+# - Create array of public addresses
+# - Clean-up code
 
 #MODULES
 from web3 import Web3, HTTPProvider
+import json
 
 #CONNECT TO NODE
 w3 = Web3(HTTPProvider('https://mainnet.infura.io/v3/9675ce340c39409d9cc2d5820ad10796'))
@@ -19,8 +23,9 @@ balance = w3.eth.get_balance('0x18aD506fFC6bD1977d94d48466680ADacf366cA4')
 print("Current Balance: ", balance / 1e18)
 
 #GET ENS TOKEN BALANCE
-
-minABI = '[{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
+f = open('minABI.json')
+data = json.load(f)
+minABI = data
 ensAddress = '0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72'
 token = w3.eth.contract(address=ensAddress, abi=minABI) # declaring the token contract
 token_balance = token.functions.balanceOf('0x18aD506fFC6bD1977d94d48466680ADacf366cA4').call() # returns int with balance, without decimals
@@ -37,14 +42,24 @@ def fetch_all_transactions(beg, end, address):
     while n < z:
       txhash = block.transactions[n].hex()
       transaction = w3.eth.getTransaction(txhash)
-      print("txhash: (", n + 1, "/", z, ") ", txhash)
+      txfrom = transaction['from']
+      txto = transaction['to']
+      if address == txfrom:
+        print("txhash from: (", n + 1, "/", z, ") ", txhash)
+      elif address == txto:
+        print("txhash to: (", n + 1, "/", z, ") ", txhash)
       n += 1
     i += 1
 
-fetch_all_transactions(14218907, 14218908, "x")
-#block = w3.eth.get_block(14218907)
-#txhash = block.transactions[0].hex()
-#print("txhash: ", txhash)
+#fetch_all_transactions(14218907, 14218908, "0x8817D887960737A604Cf712d3E5da8673DDdb7F0")
 
-#transaction = w3.eth.getTransaction(txhash)
-#print("Transaction: ", transaction)
+def test_transactions(block):
+  block = w3.eth.get_block(block)
+  txhash = block.transactions[0].hex()
+  print("txhash: ", txhash)
+
+  transaction = w3.eth.getTransaction(txhash)
+  print("Transaction From: ", transaction['from'])
+  print("Transaction To: ", transaction['to'])
+
+#test_transactions(14218907)
